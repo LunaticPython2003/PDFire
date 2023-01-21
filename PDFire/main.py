aa6e600df33ea91b7d422be1aaf7666678d19b03
pooranjoyb <pooranjoyb2016@gmail.com>
2023-01-21 15:22:21 +0530

    Setup Tailwind and Nodejs environment for Frontend

--- PDFire/main.py
+++ PDFire/main.py
@@ -1,18 +1,12 @@
-# Importing flask module in the project is mandatory
-# An object of Flask class is our WSGI application.
 from flask import Flask
- 
-# Flask constructor takes the name of
-# current module (__name__) as argument.
+from flask import render_template
+
 app = Flask(__name__)
- 
-# The route() function of the Flask class is a decorator,
-# which tells the application which URL should call
-# the associated function.
+
+# Home Route
 @app.route('/')
-# ‘/’ URL is bound with hello_world() function.
 def hello_world():
-    return 'Hello World'
+    return render_template('index.html')
  
 # main driver function
 if __name__ == '__main__':
--- README.md
+++ README.md
@@ -3,9 +3,12 @@ A PDF Converter (Flask-App)
 
 To start devlopment server 
 
+- Activate virtual environment
+- Run Flask App
 ````
 cd PDFire
 
+PDFire\Scripts\activate 
 python -m flask --app main run 
 
 USE FLAG -p 3000 for PORT
